c0209c6965294bf417a94fb598883e4c985fd144
ptajvar <ptajvar@gmail.com>
2025-05-21 10:03:05 +0200

    setting default demux memory

--- src/pixelator/pna/cli/common.py
+++ src/pixelator/pna/cli/common.py
@@ -62,7 +62,7 @@ def memory_option(func):
 
     @click.option(
         "--memory",
-        default=None,
+        default="30G",
         required=False,
         callback=_memory_validator,
         show_default=True,
