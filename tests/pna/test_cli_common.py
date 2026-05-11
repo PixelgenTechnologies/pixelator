d4bdce833d42a40a023c961dad092678cb1e93f6
Erik <erik.borgstrom@pixelgen.com>
2026-05-11 13:36:11 +0200

    chore: add panel validation test

--- tests/pna/test_cli_common.py
+++ tests/pna/test_cli_common.py
@@ -1,5 +1,8 @@
 """Copyright © 2025 Pixelgen Technologies AB."""
 
+import click
+import pytest
+
 from pixelator.pna.cli.common import validate_panel
 
 
@@ -11,3 +14,10 @@ def test_validate_panel():
 def test_validate_custom_panel(pna_data_root):
     panel_file = pna_data_root / "panels/test-pna-panel.csv"
     assert validate_panel(None, None, panel_file) == panel_file
+
+
+def test_validate_non_existing_panel():
+    """Test that validating a non-existing panel raises a UsageError."""
+    panel = "non-existing-panel"
+    with pytest.raises(click.UsageError):
+        validate_panel(None, None, panel)
