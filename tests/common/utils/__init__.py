728b37e1a8838cfe65422c4e0ed0770db40fab4b
Erik <erik.borgstrom@pixelgen.com>
2026-08-11 09:57:21 +0200

    commit import mod missed in last commit

--- tests/common/utils/__init__.py
+++ tests/common/utils/__init__.py
@@ -4,6 +4,6 @@ Tests for utility functions for the pixelator package
 Copyright © 2022 Pixelgen Technologies AB.
 """
 
-from tests.mpx.utils.utils import add_mutations, dna_seqs
+from tests.common.utils.utils import add_mutations, dna_seqs
 
 __all__ = ["add_mutations", "dna_seqs"]
